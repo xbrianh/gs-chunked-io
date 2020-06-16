238e48ba7c0f0a13d11875e9862aec06a741275a
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-06-16 10:21:19 -0600

    Fix setup.py formatting (#31)

--- setup.py
+++ setup.py
@@ -7,24 +7,25 @@ install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(
 with open("README.md") as fh:
     long_description = fh.read()
 
-setup(name='gs-chunked-io',
-      version='0.2.11',
-      description='Streaming read/writes to Google Storage blobs with ascynchronous buffering.',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/xbrianh/gs-chunked-io.git',
-      author='Brian Hannafious',
-      author_email='bhannafi@ucsc.edu',
-      license='MIT',
-      packages=find_packages(exclude=['tests']),
-      scripts=glob.glob('scripts/*'),
-      zip_safe=False,
-      install_requires=install_requires,
-      platforms=['MacOS X', 'Posix'],
-      test_suite='test',
-      classifiers=[
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.7'
-      ]
-      )
+setup(
+    name='gs-chunked-io',
+    version='0.2.11',
+    description='Streaming read/writes to Google Storage blobs with ascynchronous buffering.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/xbrianh/gs-chunked-io.git',
+    author='Brian Hannafious',
+    author_email='bhannafi@ucsc.edu',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    scripts=glob.glob('scripts/*'),
+    zip_safe=False,
+    install_requires=install_requires,
+    platforms=['MacOS X', 'Posix'],
+    test_suite='test',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
